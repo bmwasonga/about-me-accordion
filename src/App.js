ce0a0df132557a25d7ac379442cdf90c6810bd9a
bmwasonga <bmwasonga@gmail.com>
2021-04-20 14:00:18 +0300

    completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Questions';
 
 function App() {
+  const [questions, setQuestions] = useState(data);
   return (
-    <div className="App">
-      <p>Hello React</p>
+    <div className="container">
+      <h3>Questions and answers about me</h3>
+      <section className="info">
+        {questions.map((question) => {
+          return (
+            <SingleQuestion key={question.id} {...question}></SingleQuestion>
+          );
+        })}
+      </section>
+      <footer>
+        <p>A Ben Wasonga creation</p>
+      </footer>
     </div>
   );
 }
