28fafcd869aac6c7fe5ac21c340e69680dfe099f
bmwasonga <bmwasonga@gmail.com>
2021-04-20 12:28:19 +0300

    this is my first commit

---
+++ src/data.js
@@ -0,0 +1,30 @@
+const questions = [
+  {
+    id: 1,
+    title: 'What is my name?',
+    info: 'My name is Ben Wasonga',
+  },
+  {
+    id: 2,
+    title: 'Where am I from?',
+    info: 'I am a developer from Nairobi Kenya',
+  },
+  {
+    id: 3,
+    title: 'What am I?',
+    info:
+      'I am a self taught Developer with intermediate skills in HTML, JavaScript and CSS',
+  },
+  {
+    id: 4,
+    title: 'which Framework do I use?',
+    info: 'I use React framweork in all that I do',
+  },
+  {
+    id: 5,
+    title: 'What are my biggest aspirations?',
+    info:
+      'I aspire to do more in the field of tech, get to learn and grow. I aspire to build big projects and be part of something greater and good.',
+  },
+];
+export default questions;
